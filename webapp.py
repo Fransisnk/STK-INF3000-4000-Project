27e116ba3b0db592fac870285c787ec57cb380d1
fransisnk <fmkolsto@student.matnat.uio.no>
2017-01-18 17:02:48 +0100

    add init

---
+++ webapp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
