f50e1c3dc13f47c0791136df213da87fe7b0c4c2
Felix Schweinfest <fschweinfest@hotmail.com>
2017-01-19 17:46:08 +0100

    implementet search method for number of locks by ID

--- dataget.py
+++ dataget.py
@@ -1,6 +1,7 @@
-import bs4
 import re
 import json
+from pprint import pprint
+
 
 class dataget():
     def __init__(self):
@@ -13,5 +14,16 @@ class dataget():
         pass
 
     def getLocksFromJson(self, path, stationId):
-        nLocks = 0
-        return nLocks
+        with open(path) as data_file:
+            data = json.load(data_file)
+        for station in data['stations']:
+            if station['id'] == stationId:
+                return(station['number_of_locks'])
+            else:
+                return(None)
+
+''' here you can test the shit:
+a = dataget()
+print(a.getLocksFromJson('res/stations.json', 157))
+'''
+
