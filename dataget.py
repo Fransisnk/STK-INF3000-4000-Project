a5ca00196388356b7aa1e2b3b2228536377c16e0
fransisnk <fmkolsto@student.matnat.uio.no>
2017-01-23 10:37:06 +0100

    weather and lock-get fix

--- dataget.py
+++ dataget.py
@@ -18,12 +18,16 @@ class dataget():
             data = json.load(data_file)
         for station in data['stations']:
             if station['id'] == stationId:
-                return(station['number_of_locks'])
-            else:
-                return(None)
+                return station['number_of_locks']
+        return None
+
+    def getGPSFromJson(self, path, stationId):
+        pass
 
 ''' here you can test the shit:
 a = dataget()
 print(a.getLocksFromJson('res/stations.json', 157))
 '''
 
+a = dataget()
+print(a.getLocksFromJson('res/stations.json', 158))
--- weatherget.py
+++ weatherget.py
@@ -1,21 +1,35 @@
 from bs4 import BeautifulSoup as bs
 import pandas as pd
-import io
-import html5lib
-import lxml
 
+# Dependencies: html5lib, pandas, lxml
 tempHtml = "temp/temp.html"
 
 def getYrHtmlFromDate(date):
+    """
+    Takes an datetime object and returns url to yr for given day. url in the form Oslo/Oslo/Oslo/almanakk.html?dato=YYY-MM-DD
+    :param date: Datetime object
+    :return: Str, url to given day
+    """
     return "https://www.yr.no/sted/Norge/Oslo/Oslo/Oslo/almanakk.html?dato="+date
 
 def dataFromHtml(url):
-    with open(url,"r") as html:
+    """
+    Takes an url to yr, parses the html and makes an pandas data frame with the usefull information.
+    :param url: Str, link to yr for a earlier date
+    :return: pandas data frame with weather information
+    """
+    with open(url, "r") as html:
         soup = bs(html, "html.parser")
 
     soup = soup.findAll("table", {"class": "yr-table yr-table-hourly yr-popup-area"}, limit=1)
 
-    pddf = pd.read_html(str(soup[0]),header=0)
+    pddf = pd.read_html(str(soup[0]), header=0)[0]
+
+
+    pddf = pddf.drop(pddf.columns[[9, 10]], axis=1)
+    pddf.columns = ["Time", "Weather", "T Measured", "T Max", "T Min", "Rain/Snow", "Wind Mean", "Wind Strongest", "Humidity"]
+
     print(pddf)
+    return pddf
 
 dataFromHtml("temp/temp.html")
