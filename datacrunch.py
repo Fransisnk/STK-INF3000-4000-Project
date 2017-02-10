7b730661d113686e4ff263b63d5ed8da2949b49b
fransisnk <fmkolsto@student.matnat.uio.no>
2017-02-10 22:22:00 +0100

    add some db gets and loops

--- datacrunch.py
+++ datacrunch.py
@@ -4,6 +4,7 @@ import os
 import json
 from pymongo import MongoClient
 from dataget import Dataget
+from datetime import datetime
 
 class Datacrunch(Dataget):
 
@@ -16,6 +17,7 @@ class Datacrunch(Dataget):
         self.tripDb = self.client.tripDb
         self.trips = self.tripDb.trips
         self.stations = self.tripDb.stations
+        self.weather = self.tripDb.weather
 
         self.trippath = "res/trips"
         # self.tripData = pd.read_csv("res/totTrip.csv", sep=",", parse_dates=['Start time', 'End time'])
@@ -26,19 +28,21 @@ class Datacrunch(Dataget):
         :return: None
         """
         for e in self.getMonthlyTrips():
-            self.jsonToDB(e)
+            self.jsonToDB(e, self.trips)
 
-    def jsonToDB(self, dict):
+    def jsonToDB(self, dict, db, remove=None):
         """
         Takes a monthly json file from OSLO bysykkel and adds the data to the database
         :param dict: dict file
         :return: None
         """
-        data = dict["trips"]
-        self.trips.insert_many(data)
+        db.insert_many(dict)
+        if remove is not None:
+            db.update({}, {"$unset": {remove: 1}}, multi=True)
 
     def getFromDB(self):
         print(self.trips.find_one())
+        print(self.stations.find_one())
 
     def tripMerge(self):
         """
@@ -75,12 +79,53 @@ class Datacrunch(Dataget):
         self.tripData.to_csv("res/totTrip.csv", index=False)
         return self.tripData
 
-    def clearDB(self):
-        self.trips.remove()
+    def clearDB(self, db):
+        db.remove()
+
+    def jsonToDict(self, jsonPath):
+        """
+        Takes a path to a json file and returns a dict with the contents
+        :param jsonPath: str path to file
+        :return: dict with contents
+        """
+        with open(jsonPath) as data_file:
+            data = json.load(data_file)
+        return data
+
+    def forStationinDb(self):
+        for station in self.stations.find():
+            print(station)
+
+    def getFromAllStations(self, keys):
+        """
+        Takes a list of keys, loops trough the stations db, and returns a list with dictionaries with given keys
+        :param keys: list with keys
+        :return: list with dicts
+        """
+        rlist = []
+        for station in self.stations.find():
+            rdict = {}
+            for key in keys:
+                rdict[key] = station[key]
+            rlist.append(rdict)
+        return rlist
+
+    def countFlow(self):
+
+        for trip in self.trips.find():
+            outgoing = trip["start_station_id"]
+            ingoing = trip["end_station_id"]
+            print(trip["start_time"])
 
 if __name__ == "__main__":
     a = Datacrunch()
-    a.jsonToDB(a.unzip("temp/testzip.zip"))
+    a.jsonToDB(a.unzip("temp/testzip.zip")["trips"], a.trips)
+    stations = a.jsonToDict("res/stations.json")
+    a.jsonToDB(stations["stations"], a.stations, "bounds")
     #a.updateDB()
     #a.jsonToDB()
-    a.getFromDB()
+    #a.getFromDB()
+    #a.getFromAllStations(["center", "id"])
+    a.countFlow()
+    a.clearDB(a.trips)
+    a.clearDB(a.stations)
